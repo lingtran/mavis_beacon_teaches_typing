aa0dff2324acf68620e5ede7ef81b63d03a92a83
lingtran <lxiutran@gmail.com>
2016-04-27 09:03:02 -0600

    Refactor Attempt model score methods

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -7,15 +7,7 @@ class Attempt < ActiveRecord::Base
   end
 
   def point_score
-    score = 0
-    text.chars.each_with_index do |char, index|
-      score += 1 if text[index] == level.text[index]
-    end
-    score = (score.to_f/level.text.length) * 100
-
-  #   attempt_text = text.chars
-  #   level_text = level.text.chars
-  #   comparison = (attempt_text).zip(level_text)
-  #   comparison.count { |pair| pair.first == pair.last } / (level_text.length).to_f * 100
-  # end
+    paired_chars = (text.chars).zip(level.text.chars)
+    paired_chars.count { |pair| pair.first == pair.last } / (level.text.length).to_f * 100
+  end
 end
